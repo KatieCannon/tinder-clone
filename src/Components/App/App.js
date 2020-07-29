8019f075832d3cdaa1140d26a0a5481b8f1df8b4
Katie Cannon <katiecannon@Katies-MacBook-Air.local>
2020-07-29 17:25:46 +0100

    initial routing

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import Header from '../Header/Header';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 const App = () => {
   return (
     <div className="App">
       <Header />
+      <Router>
+        <Switch>
+          <Route path='/chat'>
+            <h1>chat</h1>
+          </Route>
+          <Route path='/'> 
+            <h1>homepage</h1>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
